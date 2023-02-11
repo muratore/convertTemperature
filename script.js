fb0551b865b2c7a667adb2108a5a539ff1533d5f
Alessandro Muratore <muratore@gmail.com>
2023-02-11 09:46:28 -0300

    Comentários no javascript

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ const tempInput = document.querySelector(".temp");
 const submitBtn = document.querySelector("button[type=submit]");
 const showMessage = document.querySelector(".showResult");
 // Evento de clique no botão Submit
-
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   showMessage.innerText = "";
   //Seleciona os inputs radios 
   document.querySelectorAll("input[type=radio]").forEach((item, i) => { 
-   // Verifica se é o Celsius que está Checked 
+   // Verifica se é o Celsius que está Checked no radio button
     if (item.checked && item.value === "Celsius") {
-    // Atriu a variavel a conversão de Celsius para fahrenheit
+    // Atriu a variavel tofahrenheit o valor conversão de Celsius para fahrenheit
       const tofahrenheit = (tempInput.value * 1.8 + 32)
-        .toFixed(2)
-        .replace(".", ",");
+        .toFixed(2) //limita a dois os números decimais
+        .replace(".", ",");// substitui o ponto pela vírgula - padrão brasileiro -
     // Mostra a mensagem
       showMessage.innerText = `A conversão de ${tempInput.value} ˚C são: ${tofahrenheit} ˚F fahrenheit`;
 
