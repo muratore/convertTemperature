5f949b5a5f0486a470999e0f495fd12b05697883
Alessandro Muratore <muratore@gmail.com>
2023-02-10 22:59:07 -0300

    Upload do projeto de Conversor de Temperatura

---
+++ script.js
@@ -0,0 +1,32 @@
+// Selecionar elementos da DOM
+const tempInput = document.querySelector(".temp");
+const submitBtn = document.querySelector("button[type=submit]");
+const showMessage = document.querySelector(".showResult");
+// Evento de clique no botão Submit
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  showMessage.innerText = "";
+  //Seleciona os inputs radios 
+  document.querySelectorAll("input[type=radio]").forEach((item, i) => { 
+   // Verifica se é o Celsius que está Checked 
+    if (item.checked && item.value === "Celsius") {
+    // Atriu a variavel a conversão de Celsius para fahrenheit
+      const tofahrenheit = (tempInput.value * 1.8 + 32)
+        .toFixed(2)
+        .replace(".", ",");
+    // Mostra a mensagem
+      showMessage.innerText = `A conversão de ${tempInput.value} ˚C são: ${tofahrenheit} ˚F fahrenheit`;
+
+    } else if (item.checked && item.value === "fahrenheit") {
+      console.log("fahrenheit");
+      const toCelsius = ((tempInput.value - 32) / 1.8)
+        .toFixed(2)
+        .replace(".", ",");
+      showMessage.innerText = `A conversão de ${tempInput.value} ˚F são: ${toCelsius} ˚C Celsius`;
+    }
+  });
+});
+
+// Botar limpar
+document.querySelector('button[type=reset]').addEventListener('click',()=>showMessage.innerText='')
